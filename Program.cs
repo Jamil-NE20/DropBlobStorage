7abaadc714e36719a914d6a8ee9c3993b67810fa
Md Jamil Uddin <azad1031@gmail.com>
2020-09-29 09:37:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DropBlobStorage
+{
+    class Program
+    {
+        static async Task (string[] args)
+        {
+            
+        }
+    }
+}
